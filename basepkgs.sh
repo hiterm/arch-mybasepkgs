ff2266deb195756eb4f037ed96b64e86fd3ab95e
htlsne <htlsne@users.noreply.github.com>
2017-05-03 12:21:16 +0900

    Update

--- basepkgs.sh
+++ basepkgs.sh
@@ -2,6 +2,7 @@ pacman -S zsh git gvim tmux fzf \
     xorg-server \
     gnome gnome-tweak-tool \
     adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts \
+    adobe-source-code-pro-fonts \
     ttf-dejavu ttf-liberation \
     fcitx-im fcitx-configtool fcitx-mozc \
     xclip xorg-xmodmap openssh wget curl \
